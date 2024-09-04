38e834743241352b8b247a26e77683a0b93418a0
Roland Webster <39658855+RolandMWebster@users.noreply.github.com>
2024-09-04 13:04:49 -0700

    Add nox for linting, testing, and github actions
    * Add nox to dependencies
    
    * Add noxfile.py
    
    * Add combined nox workflow file

---
+++ noxfile.py
@@ -0,0 +1,18 @@
+import nox
+
+nox.options.sessions = ["lint"]
+
+
+PYTHON_VERSIONS = ["3.9"]
+
+
+@nox.session(python=PYTHON_VERSIONS)
+def lint(session):
+    session.install("ruff==0.6.3")
+    session.run("ruff", "check")
+
+
+@nox.session(python=PYTHON_VERSIONS)
+def test(session):
+    session.install(".[test]")
+    session.run("pytest", "--cov=file_manager", "tests/")
