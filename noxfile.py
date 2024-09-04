780a7de12c42ba7cc34ad00fe1cc87f8a9c87765
Roland <rolandmwebster@gmail.com>
2024-09-04 12:57:27 -0700

    Update noxfile with lint installations

--- noxfile.py
+++ noxfile.py
@@ -8,11 +8,11 @@ PYTHON_VERSIONS = ["3.9"]
 
 @nox.session(python=PYTHON_VERSIONS)
 def lint(session):
-    session.install("ruff")
+    session.install(".[lint]")
     session.run("ruff", "check")
 
 
 @nox.session(python=PYTHON_VERSIONS)
 def test(session):
-    session.install(".[dev]")
+    session.install(".[test]")
     session.run("pytest", "--cov=file_manager", "tests/")
