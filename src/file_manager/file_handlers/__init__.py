99f04782d2d0ef557231060834e2e813eb07099f
Roland <rolandmwebster@gmail.com>
2024-09-07 18:39:53 -0700

    Add google handler to init

--- src/file_manager/file_handlers/__init__.py
+++ src/file_manager/file_handlers/__init__.py
@@ -1,6 +1,7 @@
 from file_manager.file_handlers.azure_handler import AzureHandler
 from file_manager.file_handlers.base_handler import BaseHandler
 from file_manager.file_handlers.get_handler import get_handler
+from file_manager.file_handlers.google_handler import GoogleHandler
 from file_manager.file_handlers.local_handler import LocalHandler
 from file_manager.file_handlers.s3_handler import S3Handler
 
@@ -9,5 +10,6 @@ __all__ = [
     "LocalHandler",
     "AzureHandler",
     "S3Handler",
+    "GoogleHandler",
     "get_handler",
 ]
