ebf19f04a5577bf7e134b4e71d05f3276be84c5e
Roland Webster <39658855+RolandMWebster@users.noreply.github.com>
2024-09-07 22:05:11 -0700

    Documentation/handler specific docs (#3)
    * Add implementation details to s3 handler
    
    * Add implementation details for google handler

--- src/file_manager/file_handlers/google_handler.py
+++ src/file_manager/file_handlers/google_handler.py
@@ -12,6 +12,40 @@ from file_manager.file_handlers.base_handler import BaseHandler
 class GoogleHandler(BaseHandler):
     """
     A file handler for reading and writing files to a Google Cloud Storage bucket.
+
+    Implementation Details
+    ----------------------
+    The handler uses the ``google.cloud.storage`` library to interact with Google Cloud
+    Storage. More specifically, it uses the ``google.cloud.storage.Client`` class to
+    create a client object that can be used to interact with the GCS bucket. Users can
+    supply custom arguments to the ``google.cloud.storage.Client`` instantiation by
+    providing them as a dictionary in the ``client_kwargs`` argument of the handler. This
+    is useful for supplying custom GCS credentials or other configuration options. For
+    more information on authentication to GCS via the ``google.cloud.storage.Client``
+    class, visit the google documentation on client library authentication here:
+    https://cloud.google.com/docs/authentication/client-libraries
+
+    Parameters
+    ----------
+    project : str
+        The name of the GCP project to use for authentication.
+    bucket : str
+        The name of the GCS bucket to read and write files from.
+    path_prefix : str, optional
+        A prefix to add to the start of all file paths. This can be used to store files
+        in a subdirectory within the bucket. Typically, this would be a directory name
+        that matches the project name.
+    client_kwargs : dict, optional
+        Additional keyword arguments to pass to the google.cloud.storage.Client when
+        connecting to GCS.
+
+    Examples
+    --------
+    >>> from file_manager.file_handlers import GoogleHandler
+    >>> # Setup a Google Cloud Storage handler using Application Default Credentials (ADC)
+    >>> gcs_handler = GoogleHandler(
+    ...     project="my-project", bucket="my-bucket", path_prefix="my_sub_project/"
+    ... )
     """
 
     def __init__(
