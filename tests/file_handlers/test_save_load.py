2d04501de9658f78ccb99c20801c11f19102e3f7
Roland <rolandmwebster@gmail.com>
2024-09-16 00:45:54 -0700

    Add remote pytest mark

--- tests/file_handlers/test_save_load.py
+++ tests/file_handlers/test_save_load.py
@@ -27,52 +27,52 @@ from file_manager.file_handlers import (
 
 load_dotenv()
 
-handlers = {
-    "google": GoogleHandler(
-        project=os.getenv("TESTING_GOOGLE_PROJECT"),
-        bucket=os.getenv("TESTING_GOOGLE_BUCKET"),
-        path_prefix=os.getenv("TESTING_PATH_PREFIX"),
-    ),
-    "azure": AzureHandler(
-        account_url=os.getenv("TESTING_AZURE_ACCOUNT_URL"),
-        container=os.getenv("TESTING_AZURE_CONTAINER"),
-        path_prefix=os.getenv("TESTING_PATH_PREFIX"),
-    ),
-    "s3": S3Handler(
-        bucket=os.getenv("TESTING_S3_BUCKET"),
-        path_prefix=os.getenv("TESTING_PATH_PREFIX"),
-    ),
-    "local": LocalHandler(),
-}
+
+@pytest.fixture(params=["google", "azure", "s3", "local"])
+def handler(request):
+    map = {
+        "google": GoogleHandler(
+            project=os.getenv("TESTING_GOOGLE_PROJECT"),
+            bucket=os.getenv("TESTING_GOOGLE_BUCKET"),
+            path_prefix=os.getenv("TESTING_PATH_PREFIX"),
+        ),
+        "azure": AzureHandler(
+            account_url=os.getenv("TESTING_AZURE_ACCOUNT_URL"),
+            container=os.getenv("TESTING_AZURE_CONTAINER"),
+            path_prefix=os.getenv("TESTING_PATH_PREFIX"),
+        ),
+        "s3": S3Handler(
+            bucket=os.getenv("TESTING_S3_BUCKET"),
+            path_prefix=os.getenv("TESTING_PATH_PREFIX"),
+        ),
+        "local": LocalHandler(),
+    }
+    return map[request.param]
 
 
-@pytest.mark.parametrize("handler_name", handlers.keys())
-def test_read_write_csv(handler_name, example_df, file_path):
-    handler = handlers[handler_name]
+@pytest.mark.remote
+def test_read_write_csv(example_df, file_path, handler):
     handler.save_csv(example_df, f"{file_path}.csv")
     loaded_df = handler.load_csv(f"{file_path}.csv")
     pd.testing.assert_frame_equal(example_df, loaded_df)
 
 
-@pytest.mark.parametrize("handler_name", handlers.keys())
-def test_read_write_parquet(handler_name, example_df, file_path):
-    handler = handlers[handler_name]
+@pytest.mark.remote
+def test_read_write_parquet(example_df, file_path, handler):
     handler.save_parquet(example_df, f"{file_path}.parquet")
     loaded_df = handler.load_parquet(f"{file_path}.parquet")
     pd.testing.assert_frame_equal(example_df, loaded_df)
 
 
-@pytest.mark.parametrize("handler_name", handlers.keys())
-def test_read_write_json(handler_name, example_dict, file_path):
-    handler = handlers[handler_name]
+@pytest.mark.remote
+def test_read_write_json(example_dict, file_path, handler):
     handler.save_json(example_dict, f"{file_path}.json")
     loaded_dict = handler.load_json(f"{file_path}.json")
     assert example_dict == loaded_dict
 
 
-@pytest.mark.parametrize("handler_name", handlers.keys())
-def test_read_write_pkl(handler_name, example_class, file_path):
-    handler = handlers[handler_name]
+@pytest.mark.remote
+def test_read_write_pkl(example_class, file_path, handler):
     handler.save_pickle(example_class, f"{file_path}.pkl")
     loaded_class = handler.load_pickle(f"{file_path}.pkl")
     assert example_class == loaded_class
