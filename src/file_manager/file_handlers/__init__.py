50ad55c2d1ac3ca208dfcdb25bfde9b803eb720d
Roland <rolandmwebster@gmail.com>
2024-09-15 22:17:27 -0700

    Add none handler to file handlers init

--- src/file_manager/file_handlers/__init__.py
+++ src/file_manager/file_handlers/__init__.py
@@ -3,10 +3,12 @@ from file_manager.file_handlers.base_handler import BaseHandler
 from file_manager.file_handlers.get_handler import get_handler
 from file_manager.file_handlers.google_handler import GoogleHandler
 from file_manager.file_handlers.local_handler import LocalHandler
+from file_manager.file_handlers.none_handler import NoneHandler
 from file_manager.file_handlers.s3_handler import S3Handler
 
 __all__ = [
     "BaseHandler",
+    "NoneHandler",
     "LocalHandler",
     "AzureHandler",
     "S3Handler",
